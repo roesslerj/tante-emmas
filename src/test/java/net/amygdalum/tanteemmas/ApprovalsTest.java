66278b94e075f1483455342ba7c8c79fe29b06fd
Daniel Kraus <daniel.kraus@retest.de>
2018-07-18 19:50:02 +0200

    Tweak ApprovalsTest for Travis CI

--- src/test/java/net/amygdalum/tanteemmas/ApprovalsTest.java
+++ src/test/java/net/amygdalum/tanteemmas/ApprovalsTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import net.amygdalum.tanteemmas.server.Server;
 
@@ -24,9 +25,17 @@ public class ApprovalsTest {
 
 		// If ChromeDriver is not in your PATH, uncomment this and point to your installation.
 		// e.g. it can be downloaded from http://chromedriver.chromium.org/downloads
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+		//		System.setProperty( "webdriver.chrome.driver", "path/to/chromedriver" );
 
-		driver = new ChromeDriver();
+		final ChromeOptions opts = new ChromeOptions();
+		opts.addArguments(
+				// Enable headless mode for faster execution.
+				"--headless",
+				// Use Chrome in container-based Travis CI environment (see https://docs.travis-ci.com/user/chrome#Sandboxing).
+				"--no-sandbox",
+				// Fix window size for stable results.
+				"--window-size=1200,800" );
+		driver = new ChromeDriver( opts );
 		driver.manage().timeouts().pageLoadTimeout(-1, TimeUnit.MINUTES);
 	}
 
